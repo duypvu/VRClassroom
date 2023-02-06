3ef075e69ab9af2ff4c6f2c55c5ecc8aeb42c6b6
Duy Vu <61260399+dugod@users.noreply.github.com>
2023-02-06 12:46:08 -0600

    Fixed broken marker and whiteboard prefabs

--- Assets/NetworkManager.cs
+++ Assets/NetworkManager.cs
@@ -5,16 +5,5 @@ using UnityEngine.UI;
 
 public class NetworkManager : MonoBehaviour
 {
-    [SerializeField] private Button hostBtn;
-    [SerializeField] private Button clientBtn;
-
-    private void Awake()
-    {
-        hostBtn.onClick.AddListener(() => {
-            NetworkManager.Singleton.StartServer();
-        });
-        clientBtn.onClick.AddListener(() => {
-            NetworkManager.Singleton.StartClient();
-        });
-    }
+    
 }
--- Assets/NetworkManagerUI.cs
+++ Assets/NetworkManagerUI.cs
@@ -6,17 +6,5 @@ using UnityEngine.UI;
 
 public class NetworkManagerUI : MonoBehaviour
 {
-    [SerializeField] private Button hostBtn;
-    [SerializeField] private Button clientBtn;
-
-    private void Awake()
-    {
-        hostBtn.onClick.AddListener(() => {
-            NetworkManager.Singleton.StartServer();
-            //Destroy(GameObject.FindWithTag("XROrigin"));
-        });
-        clientBtn.onClick.AddListener(() => {
-            NetworkManager.Singleton.StartClient();
-        });
-    }
+    
 }
