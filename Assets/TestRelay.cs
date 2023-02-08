7b1d3283bd56a5ac6707689a1e8ab2fb213a0ab3
Duy Vu <61260399+dugod@users.noreply.github.com>
2023-02-08 14:41:54 -0600

    Finished Relay Server script
    Finished code for server
    Added a button for creating a server
    Server starts but crashes/ or lag?

--- Assets/TestRelay.cs
+++ Assets/TestRelay.cs
@@ -12,7 +12,7 @@ using UnityEngine;
 public class TestRelay : MonoBehaviour
 {
     // Start is called before the first frame update
-    private async void Start()
+    public async void Start()
     {
         await UnityServices.InitializeAsync();
 
@@ -23,39 +23,48 @@ public class TestRelay : MonoBehaviour
         await AuthenticationService.Instance.SignInAnonymouslyAsync();
     }
 
-    private async void CreateRelay()
+    public async void CreateRelay()
     {
         try
         {
             Allocation allocation = await RelayService.Instance.CreateAllocationAsync(3);
 
-             string joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+            string joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
 
             Debug.Log(joinCode);
 
             RelayServerData relayServerData = new RelayServerData(allocation, "dtls");
             NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
 
-            NetworkManager.Singleton.
+            NetworkManager.Singleton.StartHost();
 
-        } catch(RelayServiceException e)
+
+        }
+        catch (RelayServiceException e)
         {
             Debug.Log(e);
         }
-  
+
 
     }
 
-    private async void JoinRelay(string joinCode)
+    public async void JoinRelay(string joinCode)
     {
         try
         {
             Debug.Log("Joining Relay with " + joinCode);
-            await RelayService.Instance.JoinAllocationAsync(joinCode);
-        } catch(RelayServiceException e)
+
+            JoinAllocation joinAllocation = await RelayService.Instance.JoinAllocationAsync(joinCode);
+
+            RelayServerData relayServerData = new RelayServerData(joinAllocation, "dtls");
+
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
+            NetworkManager.Singleton.StartClient();
+
+        }
+        catch (RelayServiceException e)
         {
             Debug.Log(e);
         }
     }
-
 }
